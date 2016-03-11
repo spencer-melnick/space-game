1e096efa25f16a4cb160c2739e73dbf4fa3eadf8
spencer-melnick <smelnick97@gmail.com>
2016-03-11 16:59:20 -0500

    Added basic ObjectPool implementation

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "system/window.h"
 #include "math/vector2d.h"
 #include "system/resource.h"
+#include "system/pool.h"
 #include "graphics/texture.h"
 
 using namespace Game;
@@ -37,6 +38,30 @@ int main()
     auto texture1 = textureHandle1.getData<Texture>();
     Texture::ErrorCode loadError = texture1->loadFromFile("./rc/texture.png", window.getSdlRenderer());
 
+
+    auto poolData = new ObjectPool<int>::Object[10];
+    ObjectPool<int> pool(poolData, 10);
+
+    for (int i = 0; i < 10; i ++)
+    {
+        auto a = pool.createObject();
+        if (a.isValid())
+            *(a.getData()) = i;
+    }
+
+    auto i = pool.getUsedObjects();
+    auto j = i.getNextObject().getNextObject().getNextObject();
+
+    pool.freeObject(j);
+
+    i = pool.getUsedObjects();
+    while (i.isValid())
+    {
+        std::cout << *(i.getData()) << " ";
+        i = i.getNextObject();
+    }
+
+
     SDL_Rect position({0, 0, texture1->dimensions.x, texture1->dimensions.y});
 
     SDL_RenderClear(window.getSdlRenderer());
@@ -47,6 +72,7 @@ int main()
         SDL_RenderCopy(window.getSdlRenderer(), texture1->textureSdl, nullptr, &position);
 
     SDL_RenderPresent(window.getSdlRenderer());
+
 
     SDL_Delay(3000);
 
---
+++ src/system/pool.h
@@ -0,0 +1,134 @@
+#pragma once
+
+namespace Game
+{
+    template <typename T>
+    class ObjectPool
+    {
+        public:
+            struct Object
+            {
+                T data;
+                Object* nextFree = nullptr;
+                Object* nextUsed = nullptr;
+                bool used = false;
+            };
+
+            class Handle
+            {
+                friend class ObjectPool;
+
+                public:
+                    Handle(Object* object) : _object(object)
+                        {};
+                    Handle() : _object(nullptr)
+                        {};
+
+                    bool isValid() const
+                    {
+                        if (_object == nullptr)
+                            return false;
+                        return _object->used;
+                    };
+
+                    T* getData() const
+                    {
+                        if (isValid())
+                            return &(_object->data);
+                        return nullptr;
+                    }
+
+                    Handle getNextObject() const
+                    {
+                        if (isValid())
+                            if (_object->nextUsed != nullptr)
+                                return Handle(_object->nextUsed);
+
+                        return Handle();
+                    };
+
+                protected:
+                    Object* _object;
+            };
+
+            ObjectPool(Object* data, size_t numObjects) : _objects(data), _numObjects(numObjects)
+            {
+                _firstFree = &(_objects[0]);
+
+                for (size_t i = 0; i < _numObjects - 1; i ++)
+                    _objects[i].nextFree = &(_objects[i + 1]);
+            }
+
+            Handle createObject()
+            {
+                if (_firstFree == nullptr)
+                    return Handle();
+
+                //pop the front of the free list
+                Object* temp = _firstFree;
+                _firstFree = temp->nextFree;
+
+                //add to the front of the used list
+                temp->nextUsed = _firstUsed;
+                _firstUsed = temp;
+
+                temp->used = true;
+
+                return Handle(temp);
+            }
+
+            void freeObject(Handle& handle)
+            {
+                if (handle.isValid())
+                {
+                    Object* temp = handle._object;
+
+                    //add to the free list
+                    temp->nextFree = _firstFree;
+                    _firstFree = temp;
+
+                    //remove from used
+                    if (temp->used)
+                    {
+                        if (_firstUsed == temp)
+                        {
+                            _firstUsed = temp->nextUsed;
+                            temp->nextUsed = nullptr;
+                        }
+                        else
+                        {
+                            Object* i = _firstUsed;
+                            Object* next;
+                            while (i != nullptr)
+                            {
+                                next = i->nextUsed;
+                                if (next == temp)
+                                {
+                                    auto d = temp->nextUsed;
+                                    i->nextUsed = temp->nextUsed;
+                                    temp->nextUsed = nullptr;
+                                    break;
+                                }
+                                i = i->nextUsed;
+                            }
+                        }
+
+                        temp->used = false;
+                    }
+                }
+            }
+
+            Handle getUsedObjects() const
+            {
+                if (_firstUsed == nullptr)
+                    return Handle();
+                return Handle(_firstUsed);
+            }
+
+        private:
+            Object* _objects;
+            size_t _numObjects;
+            Object* _firstFree = nullptr;
+            Object* _firstUsed = nullptr;
+    };
+}
